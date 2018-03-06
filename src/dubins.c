a53eefaa4e825ee6a43b892bced40c6a1efa7d33
Ruixiang Du <ruixiang.du@gmail.com>
2018-03-06 17:52:53 -0500

    updated include structure for easy integration

--- src/dubins.c
+++ src/dubins.c
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#include "dubins.h"
+#include "dubins/dubins.h"
 #include <math.h>
 #include <assert.h>
 
